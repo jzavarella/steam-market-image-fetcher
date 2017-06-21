6fcd0ac5580dc40545e07d6e259e8854c763dfa4
Zavarella <if993883@canada01.ifdsnet.int>
2017-06-21 11:54:40 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const htmlparser = require('htmlparser');
+const axios = require('axios');
+
+/**
+ * Retrieve image for an item.
+ *
+ * @param {number} appid - Steam application id
+ * @param {String} name - Item name, market hashed
+ * @param {Function} callback - Callback function(err, data)
+ */
+exports.getItemPrice = (appid, name, callback) => {
+    if (typeof callback !== 'function') {
+        throw new Error('No callback supplied');
+    }
+
+    axios.get(`http://steamcommunity.com/market/listings/${appid}/${name}`)
+      .then((response) => {
+        const handler = new htmlparser.DefaultHandler((error, dom) => {
+            if (error) {
+              callback(error);
+            } else {
+              const img = dom[2].children[3].children[1].children[13].children[5].children[9].children[3].children[1].children[1].attribs.src;
+              callback(img);
+            }
+        });
+        var parser = new htmlparser.Parser(handler);
+        parser.parseComplete(response);
+      })
+      .catch(err => callback(err));
+};
