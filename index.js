c90633c7726afada7973f90a436d80f003eb99b6
Zavarella <if993883@canada01.ifdsnet.int>
2017-06-21 11:58:55 -0400

    Housekeeping

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const axios = require('axios');
  * @param {String} name - Item name, market hashed
  * @param {Function} callback - Callback function(err, data)
  */
-exports.getItemPrice = (appid, name, callback) => {
+exports.getItemImage = (appid, name, callback) => {
     if (typeof callback !== 'function') {
         throw new Error('No callback supplied');
     }
@@ -16,14 +16,14 @@ exports.getItemPrice = (appid, name, callback) => {
     axios.get(`http://steamcommunity.com/market/listings/${appid}/${name}`)
       .then((response) => {
         const handler = new htmlparser.DefaultHandler((error, dom) => {
-            if (error) {
-              callback(error);
-            } else {
-              const img = dom[2].children[3].children[1].children[13].children[5].children[9].children[3].children[1].children[1].attribs.src;
-              callback(img);
-            }
+          if (error) {
+            callback(error);
+          } else {
+            const img = dom[2].children[3].children[1].children[13].children[5].children[9].children[3].children[1].children[1].attribs.src;
+            callback(img);
+          }
         });
-        var parser = new htmlparser.Parser(handler);
+        const parser = new htmlparser.Parser(handler);
         parser.parseComplete(response);
       })
       .catch(err => callback(err));
